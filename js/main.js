458969e571ccc0d001088c1c19bf218f5b9ee2cb
Rene B <thckrypto@gmail.com>
2022-04-02 14:53:37 +0200

    Added Hover-Effect to the Grid

--- js/main.js
+++ js/main.js
@@ -8,4 +8,10 @@ function squareGrid() {
     }
 }
 
+
 squareGrid();
+
+const square = document.querySelector("div");
+square.addEventListener("mouseover", function(e) {
+    e.target.classList.replace("square", "color");
+});
