da5cdd6770df605c53f088f76f06a66c9d0a1874
Rene B <thckrypto@gmail.com>
2022-04-02 14:21:46 +0200

    Written function to create squares and styled them to make them visible

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const grid = document.querySelector(".grid");
+
+function squareGrid() {
+    for (let i = 0; i < 256; i++) {
+        const square = document.createElement("div");
+        square.classList.add("square");
+        grid.appendChild(square);
+    }
+}
+
+squareGrid();
